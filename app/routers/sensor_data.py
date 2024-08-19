1df47d963e1e5e76421c44caadd567854c48a4d1
Johan Guse <johanguse@gmail.com>
2024-08-18 22:58:38 -0300

    feat: add upload-csv endpoint

--- app/routers/sensor_data.py
+++ app/routers/sensor_data.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
+import csv
+from datetime import datetime
+from io import StringIO
+
+import chardet
+import pandas as pd
+from fastapi import APIRouter, Depends, File, HTTPException, Query, UploadFile
 from fastapi_pagination import LimitOffsetPage
 from sqlalchemy.orm import Session
 
-from app.core.auth import get_current_user
+from app.core.auth import get_current_admin_user, get_current_user
 from app.core.database import get_db
 from app.models.equipment import Equipment
 from app.models.sensor_data import SensorData as SensorDataModel
@@ -55,3 +61,109 @@ def read_sensor_data(
     return LimitOffsetPage(
         items=items, total=total, limit=limit, offset=offset
     )
+
+
+def detect_delimiter(file_content: str):
+    sniffer = csv.Sniffer()
+    try:
+        dialect = sniffer.sniff(file_content)
+        return dialect.delimiter
+    except csv.Error:
+        return ','
+
+
+@router.post('/upload-csv/')
+async def upload_csv(
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_admin_user),
+):
+    if not file.filename.endswith('.csv'):
+        raise HTTPException(
+            status_code=400,
+            detail='File format not supported. Please upload a CSV file.',
+        )
+
+    contents = await file.read()
+    result = chardet.detect(contents)
+    file_encoding = (
+        result['encoding'] if result['encoding'] is not None else 'utf-8'
+    )
+
+    try:
+        decoded_contents = contents.decode(file_encoding)
+        delimiter = detect_delimiter(decoded_contents)
+        df = pd.read_csv(StringIO(decoded_contents), delimiter=delimiter)
+        df.columns = df.columns.str.strip()
+
+        required_columns = ['equipmentId', 'timestamp', 'value']
+        if not all(column in df.columns for column in required_columns):
+            raise HTTPException(
+                status_code=400,
+                detail=f"Missing required columns: {', '.join(required_columns)}",
+            )
+
+        sensor_data_list = []
+        for index, row in df.iterrows():
+            try:
+                timestamp_dt = datetime.strptime(
+                    row['timestamp'].strip(), '%Y-%m-%dT%H:%M:%S.%f%z'
+                )
+
+                # Check if the equipment exists and if the user has access to it
+                equipment = (
+                    db.query(Equipment)
+                    .filter(Equipment.equipment_id == row['equipmentId'])
+                    .first()
+                )
+                if not equipment:
+                    raise HTTPException(
+                        status_code=400,
+                        detail=f"Equipment with ID {row['equipmentId']} not found",
+                    )
+
+                # Check if the user has access to the equipment's company
+                user_company_relation = (
+                    db.query(user_company)
+                    .filter(
+                        user_company.c.user_id == current_user.id,
+                        user_company.c.company_id == equipment.company_id,
+                    )
+                    .first()
+                )
+                if not user_company_relation:
+                    raise HTTPException(
+                        status_code=403,
+                        detail=f"You don't have access to equipment {row['equipmentId']}",
+                    )
+
+                sensor_data = SensorDataModel(
+                    equipment_id=equipment.id,
+                    timestamp=timestamp_dt,
+                    value=float(row['value']),
+                )
+                sensor_data_list.append(sensor_data)
+            except ValueError as ve:
+                raise HTTPException(
+                    status_code=400,
+                    detail=f'Error processing row {index}: {str(ve)}',
+                )
+
+        db.bulk_save_objects(sensor_data_list)
+        db.commit()
+
+    except UnicodeDecodeError:
+        raise HTTPException(
+            status_code=400,
+            detail='Could not decode file content. Please ensure the file is properly encoded.',
+        )
+    except pd.errors.ParserError:
+        raise HTTPException(
+            status_code=400,
+            detail='Error parsing the CSV file. Please check the file format.',
+        )
+
+    return {
+        'detail': 'File processed successfully',
+        'sensors_added': len(sensor_data_list),
+    }
